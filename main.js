146ed4a6019dd32c626bb4ac4b0b925a9addeea5
Mrinank Pandey <79464354+Mkpy10@users.noreply.github.com>
2022-06-07 16:25:39 +0100

    Add files via upload

--- main.js
+++ main.js
@@ -21,15 +21,29 @@ var ball = {
     dy:3
 }
 
+function preload(){
+
+}
+
 function setup(){
-  var canvas =  createCanvas(700,600);
-  canvas.center()
+  canvas =  createCanvas(700,600);
+  canvas.parent('canvas');
+  video = createCapture(VIDEO);
+  video.size(700, 600);
+  video.hide();
+  PoseNet = ml5.poseNet(video, modelLoaded);
+  
+}
+
+function modelLoaded(){
+  console.log("Model Loaded!");
 }
 
 
 function draw(){
 
  background(0); 
+ image(video, 0, 0, 700, 600);
 
  fill("black");
  stroke("black");
